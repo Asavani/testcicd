2ed646d5dbe2fbf742549961af3763de2d1a7b7a
anilappjojo <asavani.anil@gmail.com>
2025-05-18 16:19:22 +0530

    Changes Made

--- myController/normal.js
+++ myController/normal.js
@@ -4,6 +4,6 @@ router.get("/",async(req,res)=>{
     res.send({"status":2000,"msg":"Hello I am CI CD"});
 });
 router.get("/data",async(req,res)=>{
-    res.send({"status":2000,"msg":"Hello Data Details PJP"});
+    res.send({"status":2000,"msg":"Hello Data Details Mama"});
 });
 module.exports=router;
