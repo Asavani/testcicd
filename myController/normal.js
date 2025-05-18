70b0654048a513fe581ebd5f4ee179239e3e015e
anilappjojo <asavani.anil@gmail.com>
2025-05-18 16:34:51 +0530

    Updated

--- myController/normal.js
+++ myController/normal.js
@@ -1,9 +1,9 @@
 let express=require("express");
 let router=express.Router();
 router.get("/",async(req,res)=>{
-    res.send({"status":2000,"msg":"Hello I am CI CD"});
+    res.send({"status":2000,"msg":"Hello I am CI CD PIPE LINE"});
 });
 router.get("/data",async(req,res)=>{
-    res.send({"status":2000,"msg":"Hello Data Details"});
+    res.send({"status":2000,"msg":"Hello Data Details Coming"});
 });
 module.exports=router;
