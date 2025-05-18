5fae4112808c5205c40a6a1b3f4e797770a17c73
anilappjojo <asavani.anil@gmail.com>
2025-05-18 16:58:27 +0530

    test

--- myController/normal.js
+++ myController/normal.js
@@ -4,6 +4,6 @@ router.get("/",async(req,res)=>{
     res.send({"status":2000,"msg":"Hello I am CI CD PIPE"});
 });
 router.get("/data",async(req,res)=>{
-    res.send({"status":2000,"msg":"Hello Data Details Coming"});
+    res.send({"status":2000,"msg":"Hello Data Details Get"});
 });
 module.exports=router;
