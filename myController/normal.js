ca0022adf8fb8f02f98cbdef77ee5c429564b712
anilappjojo <anilasavani@appjojo.in>
2025-05-18 14:40:20 +0530

    Message Changed

--- myController/normal.js
+++ myController/normal.js
@@ -4,6 +4,6 @@ router.get("/",async(req,res)=>{
     res.send({"status":2000,"msg":"Hello I am CI CD"});
 });
 router.get("/data",async(req,res)=>{
-    res.send({"status":2000,"msg":"Hello Data Received Data"});
+    res.send({"status":2000,"msg":"Hello Data Received Data And Updated"});
 });
 module.exports=router;
