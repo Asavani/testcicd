48811f5928c7d33b0a2c16754c38fe8cf3d93dd0
anilappjojo <anilasavani@appjojo.in>
2025-05-18 14:02:58 +0530

    Controller File Changed

--- myController/normal.js
+++ myController/normal.js
@@ -3,4 +3,7 @@ let router=express.Router();
 router.get("/",async(req,res)=>{
     res.send({"status":2000,"msg":"Hello I am CI CD"});
 });
+router.get("/data",async(req,res)=>{
+    res.send({"status":2000,"msg":"Hello Data Received"});
+});
 module.exports=router;
