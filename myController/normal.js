1137daa7a4f2059f077bb995d7fbe0a857115c21
anilappjojo <asavani.anil@gmail.com>
2025-05-18 19:56:52 +0530

    Code Changes

--- myController/normal.js
+++ myController/normal.js
@@ -3,7 +3,5 @@ let router=express.Router();
 router.get("/",async(req,res)=>{
     res.send({"status":2000,"msg":"Hello I am CI CD PIPE"});
 });
-router.get("/data",async(req,res)=>{
-    res.send({"status":2000,"msg":"Hello Data Details Get"});
-});
+
 module.exports=router;
