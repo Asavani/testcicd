dbbfe2caec6c7ea4fc8203be8116ab0b359950b4
anilappjojo <asavani.anil@gmail.com>
2025-05-18 20:28:49 +0530

    Controller Added

--- myController/normal.js
+++ myController/normal.js
@@ -1,7 +1,12 @@
 let express=require("express");
 let router=express.Router();
+let student=[{"id":1,"sname":"Anil"},{"id":2,"sname":"Sunil"}];
 router.get("/",async(req,res)=>{
     res.send({"status":2000,"msg":"Hello I am CI CD PIPE"});
+});
+
+router.get("/getdata",async(req,res)=>{
+    res.send({"status":2000,data:student});
 });
 
 module.exports=router;
